680b5c949e05dbb97ba39767d04bc03c6ce39eb1
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2021-08-24 11:34:06 -0400

    update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World
         </a>
       </header>
     </div>
