f13fb7558166784b5e5c5c4ec1a2daa4a1ea0422
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2021-09-05 02:04:23 -0400

    add remove contact logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
 
 class App extends Component {
+  state = {
+    contacts : [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  }
+
+  // everytime we remove a contact, it shoudl update contacts array 
+  removeContact = (c) => {
+
+    this.setState((currentState) => ({
+      contacts : currentState.contacts.filter((contact) => {
+        return contact.id !== c.id
+      })
+    }))
+  }
+  
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts = {this.state.contacts}
+        onDeleteContact = {this.removeContact}
+        ></ListContacts>
       </div>
     );
   }
