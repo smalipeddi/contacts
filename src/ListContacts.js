6ac0103537e8dd2acb60044ed7750d40e7ce207c
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2021-09-06 14:44:58 -0400

    add state to component and update state with setstate

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 class ListContacts extends Component {
-   
+
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact : PropTypes.func.isRequired
+  }
+  
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+    
+  }
+
   render() {
+    const { query } =  this.state
+    const { contacts, onDeleteContact } = this.props
     console.log('props', this.props)
+
+    const showingContacts = query === '' 
+                           ? contacts 
+                           : contacts.filter((c) => (c.name.toLowerCase().includes(query.toLowerCase())))
+    
+    
     return (
-      <ol className='contact-list'>
-        {this.props.contacts.map((contact) => (
+      <div className="list-contacts">
+    
+        <div className="list-contacts-top">
+          <input  className="search-contacts" type="text" placeholder="Search Contacts"
+          value={query}
+          onChange={(event) => this.updateQuery(event.target.value)}/>
+        </div>
+        <ol className='contact-list'>
+        {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div 
               className='contact-avatar'
@@ -16,13 +49,13 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.handle}</p>
               </div>
-              <div onClick= {() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              <div onClick= {() => onDeleteContact(contact)} className='contact-remove'>
               </div>
             </li>
         ))}
       </ol>
+      </div>
     )
   }
 }
-
 export default ListContacts
